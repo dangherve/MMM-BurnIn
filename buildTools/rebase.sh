a86ddd0cde6f4c0a96e4df124c2c131dd08017ee
dangherve <dangherve@gmail.com>
2026-03-12 13:49:34 -0400

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+git remote add upstream https://github.com/werthdavid/MMM-BurnIn.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/MMM-BurnIn.git
+git push --force --set-upstream origin master
